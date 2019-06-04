2afd25bd5266e5ab2556a7e4cb8435e5e90a31f0
prakhar.parwal <prakhar.parwal@M5-D-5Z3MXH2.dir.svc.accenture.com>
2019-06-04 12:52:32 +0530

    Added desciption: Topic: Covariant return type

--- CoreJava/src/com/prakhar/parwal/covariant_return_type/Test.java
+++ CoreJava/src/com/prakhar/parwal/covariant_return_type/Test.java
@@ -4,6 +4,16 @@ public class Test {
 	
 }
 
+
+/*
+ * 
+ * When a method overrides method of base case and the return type of overridden method 
+ * is sub class of return type class of method from parent class then the return type 
+ * of sub class method is class "Covariant return type". It came in picture from JDK5
+ * 
+ * */
+
+
 class Parent 
 {
 	public ParentReturnType myMethod()
