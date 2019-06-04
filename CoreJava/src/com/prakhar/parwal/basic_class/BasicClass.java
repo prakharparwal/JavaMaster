f4dc342742f8b1d4afc6af5c8d9e81fc5588ee71
prakhar.parwal <prakhar.parwal@M5-D-5Z3MXH2.dir.svc.accenture.com>
2019-06-04 12:00:40 +0530

    Sequence 1. Basic class example

---
+++ CoreJava/src/com/prakhar/parwal/basic_class/BasicClass.java
@@ -0,0 +1,26 @@
+package com.prakhar.parwal.basic_class;
+
+public class BasicClass {
+	//Basic class without member variables and member functions.
+}
+
+
+//Class with variables 
+class First 
+{
+	private int firstVariable;
+	private String secondVariable;
+}
+
+
+//Class with variables and methods
+class Second 
+{
+	private int firstVariable;
+	private String secondVariable;
+	
+	public void printMessage(String message)
+	{
+		System.out.println(message);
+	}
+}
---
+++ CoreJava/src/com/prakhar/parwal/basic_class/InvalidClassExamples.java
@@ -0,0 +1,27 @@
+package com.prakhar.parwal.basic_class;
+
+public class InvalidClassExamples {
+
+}
+
+
+/*
+ * ####
+ * 
+ * Uncomment the below code and the code will not compile
+ * As 'Private' and 'Protected' class is not allowed
+ * 
+ * */
+
+/*
+private class TestPrivateClass
+{
+	
+}
+
+protected class TestProtectedClass 
+{
+	
+}
+*/
+
