c8eadb919a90abeccd68386a1a80a839c01584ec
prakharparwal <none@none>
2019-06-04 23:48:59 +0530

    Spring core: Committing project

---
+++ SpringCore/spring_core_master/src/main/java/spring/spring_core_master/App.java
@@ -0,0 +1,13 @@
+package spring.spring_core_master;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
