6dd1669d934c7c6e57df90b8e4d372ba9f3cfe90
prakharparwal <none@none>
2019-06-05 09:37:57 +0530

    Committing spring core first example

---
+++ SpringCore/spring_core_master/src/main/java/com/prakhar/parwal/data/Person.java
@@ -0,0 +1,5 @@
+package com.prakhar.parwal.data;
+
+public class Person {
+
+}
--- SpringCore/spring_core_master/src/main/java/spring/spring_core_master/App.java
+++ SpringCore/spring_core_master/src/main/java/spring/spring_core_master/App.java
@@ -1,13 +1,20 @@
 package spring.spring_core_master;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.prakhar.parwal.data.Person;
+
 /**
- * Hello world!
+ * 
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+	
+    public static void main( String[] args ) {
+    	
+    	ApplicationContext appContext = new ClassPathXmlApplicationContext("spring-bean-configuration.xml");
+    	Person e1 = (Person) appContext.getBean("personBean");
+    	System.out.print(e1);
     }
 }
