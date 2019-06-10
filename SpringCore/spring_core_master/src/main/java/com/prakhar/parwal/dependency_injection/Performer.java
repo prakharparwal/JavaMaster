8cd87634da0741aa0644296fb59e1da2d4de5317
prakhar.parwal <prakhar.parwal@M5-D-5Z3MXH2.dir.svc.accenture.com>
2019-06-10 16:44:26 +0530

    Spring_Core: Dependency Injection _1

---
+++ SpringCore/spring_core_master/src/main/java/com/prakhar/parwal/dependency_injection/Performer.java
@@ -0,0 +1,14 @@
+package com.prakhar.parwal.dependency_injection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Performer {
+
+	public static void main(String... q)
+	{
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/prakhar/parwal/dependency_injection/spring-bean-configuration-dependency_injection.xml");
+		Person p = (Person) ctx.getBean("personBean");
+		System.out.println(p);
+	}
+}
---
+++ SpringCore/spring_core_master/src/main/java/com/prakhar/parwal/dependency_injection/Person.java
@@ -0,0 +1,35 @@
+package com.prakhar.parwal.dependency_injection;
+
+public class Person {
+
+	private int id;
+	private String name;
+	private Address address;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return this.id+this.name+this.address;
+	}
+	
+}
