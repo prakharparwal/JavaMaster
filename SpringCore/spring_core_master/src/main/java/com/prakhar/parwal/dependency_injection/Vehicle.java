8d0409844d06e3f694965831a237015801afb095
prakhar.parwal <prakhar.parwal@M5-D-5Z3MXH2.dir.svc.accenture.com>
2019-06-10 18:24:11 +0530

    Spring_Core Dependency Injection: Collection

---
+++ SpringCore/spring_core_master/src/main/java/com/prakhar/parwal/dependency_injection/Vehicle.java
@@ -0,0 +1,22 @@
+package com.prakhar.parwal.dependency_injection;
+
+public class Vehicle {
+
+	private int type;
+	private String name;
+	
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
