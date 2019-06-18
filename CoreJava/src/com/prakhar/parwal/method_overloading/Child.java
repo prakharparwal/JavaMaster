1360e37ccd5f2b9548a9752b731bfafbb113c8c7
prakhar.parwal <prakhar.parwal@M5-D-5Z3MXH2.dir.svc.accenture.com>
2019-06-18 12:03:55 +0530

    Core Java: Method overloading [partial]

---
+++ CoreJava/src/com/prakhar/parwal/method_overloading/Child.java
@@ -0,0 +1,5 @@
+package com.prakhar.parwal.method_overloading;
+
+public class Child extends Parent {
+
+}
---
+++ CoreJava/src/com/prakhar/parwal/method_overloading/Performer.java
@@ -0,0 +1,5 @@
+package com.prakhar.parwal.method_overloading;
+
+public class Performer {
+
+}
