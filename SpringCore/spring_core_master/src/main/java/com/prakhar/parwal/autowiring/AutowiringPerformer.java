f7f26289bdfa146b3d3280e345749e6ddc6a0242
prakhar.parwal <prakhar.parwal@M5-D-5Z3MXH2.dir.svc.accenture.com>
2019-06-18 11:53:29 +0530

    Spring_Core Autowiring [partial]

--- SpringCore/spring_core_master/src/main/java/com/prakhar/parwal/autowiring/AutowiringPerformer.java
+++ SpringCore/spring_core_master/src/main/java/com/prakhar/parwal/autowiring/AutowiringPerformer.java
@@ -1,9 +1,14 @@
 package com.prakhar.parwal.autowiring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AutowiringPerformer {
 
 	public static void main(String... q) {
 		
-		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/prakhar/parwal/autowiring/spring-bean-config-autowiring.xml");
+		Employee emp = (Employee) context.getBean("employeeBean");
+		System.out.println(emp);
 	}
 }
