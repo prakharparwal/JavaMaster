6bbcf456a4065970f3b6a36b5971c964fefdbf6e
prakhar.parwal <prakhar.parwal@M5-D-5Z3MXH2.dir.svc.accenture.com>
2019-07-01 11:02:53 +0530

    Spring: Spring MVC - Partial submission _ First example

---
+++ spring_mvc_master/src/com/prakhar/parwal/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.prakhar.parwal.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+
+	
+	@RequestMapping(value = "/test", method = RequestMethod.GET )
+	public ModelAndView test(HttpServletRequest request, HttpServletResponse response) {
+		
+		System.out.println("Request came at TestController.test()");
+		
+		return new ModelAndView();
+	}
+}
