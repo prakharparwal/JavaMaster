557d1508d46168fe67fa5464abebb367757c60c7
prakhar.parwal <prakhar.parwal@M5-D-5Z3MXH2.dir.svc.accenture.com>
2019-07-30 13:21:57 +0530

    Spring > MVC > Interceptors

--- Spring/MVC/master/src/com/prakhar/parwal/interceptors/BaseInterceptor.java
+++ Spring/MVC/master/src/com/prakhar/parwal/interceptors/BaseInterceptor.java
@@ -1,7 +1,17 @@
 package com.prakhar.parwal.interceptors;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.servlet.HandlerInterceptor;
 
 public class BaseInterceptor implements HandlerInterceptor {
 
+	
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		System.out.println("Base interceptor: preHandle");
+		return true;
+	}
 }
--- Spring/MVC/master/src/com/prakhar/parwal/interceptors/SignUpInterceptor.java
+++ Spring/MVC/master/src/com/prakhar/parwal/interceptors/SignUpInterceptor.java
@@ -3,18 +3,34 @@ package com.prakhar.parwal.interceptors;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class SignUpInterceptor {
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+public class SignUpInterceptor extends HandlerInterceptorAdapter  {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		System.out.println("Pre handle");
+		return true;
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			@Nullable ModelAndView modelAndView) throws Exception {
 		
-		System.out.println("Pre handler");
-		return true;
+		System.out.println("Post handle");
 	}
 	
-	public boolean postHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-	
-		System.out.println("Post handler");
-		return true;
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+			@Nullable Exception ex) throws Exception {
+		
+		System.out.println("After completion");
 	}
+
 	
 }
